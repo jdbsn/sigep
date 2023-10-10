4e8a589c3696d87ee6b8583ade2aec6b0c4a44ae
João Neto <jbsn1@discente.ifpe.edu.br>
2023-10-10 19:25:23 -0300

    commit inicial

---
+++ src/test/java/com/sigep/SigepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sigep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
